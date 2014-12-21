22a6b855282c14cb333d7e609f00b683330314b7
VMarshinskii <marshinskii@gmail.com>
2014-12-21 19:05:43 +0500

    Добавлены классы Юзера и Стола (User and Table)

---
+++ FoolService/Table.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoolService
+{
+    public class Table
+    {
+        int max_users = 6;
+        
+        // козырь
+        int trump = 0;
+
+        public List<User> users = new List<User>();
+
+        public bool add_user(User new_user)
+        {
+            if(this.users.Count < this.max_users)
+            {
+                this.users.Add(new_user);
+                return true;
+            }
+            return false;
+        }
+
+        public void exit_user(User current_user)
+        {
+            foreach(var user in this.users)
+            {
+                if (user.id == current_user.id)
+                    this.users.Remove(user);
+            }
+        }
+
+        public void play_game()
+        {
+            //Берём колоду карт, перемешиваем её
+            
+
+            //В цикле проходим по всем юзерам и раздаём по 6 карт
+
+            //Рандомно назначаем того, кто ходит
+        }
+    }
+}
---
+++ FoolService/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoolService
+{
+    public class User
+    {
+        public int id;
+
+        public string name;
+
+        //rename on Cards type
+        public List<string> cards = new List<string>();
+    }
+}
